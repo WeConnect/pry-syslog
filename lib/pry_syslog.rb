6d9b30c402fc809cbe8146fb7c713fb4543d4db1
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2015-04-20 10:49:15 -0400

    Technically working... could use some love still

--- lib/pry_syslog.rb
+++ lib/pry_syslog.rb
@@ -1,9 +1,26 @@
 require 'pry'
-require 'pry_syslog/syslog_hook'
 require 'pry_syslog/version'
+require 'syslog/logger'
 
 # Parent namespace of the pry-syslog gem.
 module PrySyslog
+  def self.announce_logging
+    puts 'All commands executed in this session will be logged to syslog.'
+  end
+
+  def self.log_code_execution(code)
+    syslog.info(code)
+  end
+
+  def self.syslog
+    @syslog ||= ::Syslog::Logger.new('pry_history', Syslog::LOG_LOCAL1)
+  end
 end
 
-Pry::History.send(:include, PrySyslog::SyslogHook)
+Pry.config.hooks.add_hook(:before_session, :logged_warning) do
+  PrySyslog.announce_logging
+end
+
+Pry.config.hooks.add_hook(:before_eval, :log_code_execution) do |code|
+  PrySyslog.log_code_execution(code)
+end
